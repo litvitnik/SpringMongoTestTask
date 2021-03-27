838c399ffbe47c92ca243b2bf183471109121ef6
Vitaliy Litunovskiy <litunovskiy.vitaliy@yandex.ru>
2021-03-28 00:42:04 +0300

    3 tests added

--- src/test/java/ru/litvitnik/testtask/controllers/UserControllerTest.java
+++ src/test/java/ru/litvitnik/testtask/controllers/UserControllerTest.java
@@ -1,7 +1,7 @@
 package ru.litvitnik.testtask.controllers;
 
 
-import org.junit.jupiter.api.Disabled;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,8 +12,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
-
+import ru.litvitnik.testtask.entities.User;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,57 +30,55 @@ class UserControllerTest {
 
     @Test
     public void getAllUsersShouldReturnOkStatus() throws Exception {
-        //this.mockMvc.perform(get("/users")).andDo(print()).andExpect(status().isOk());
         String uri = "/users";
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)
                 .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
         int status = mvcResult.getResponse().getStatus();
         assertEquals(200, status, "getAllUsers must return 200 status!");
-        String content = mvcResult.getResponse().getContentAsString();
-        //assertTrue(userList.length > 0);
+
     }
     @Test
     public void postWorksFine() throws Exception {
-        //this.mockMvc.perform(post("/users?name=VeryVeryTestName")).andExpect(status().is(HttpStatus.CREATED.value()));
-
         String uri = "/users?name=VeryVeryTestUser";
-        //User testUser = new User("VeryVeryTestPerson");
-        //String inputJson = mapToJson(testUser);
         MvcResult mvcResult = mockMvc
-                .perform(MockMvcRequestBuilders.post(uri)
+                .perform(post(uri)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
         int status = mvcResult.getResponse().getStatus();
-        assertEquals(201, status);
-        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(201, status, "addUser should return 201 status!");
         String location = mvcResult.getResponse().getHeader("Location");
         System.out.println(location);
-        //assertEquals(content, "Product is created successfully");
+        assertNotNull(location, "addUser should return location of new user!");
+        mockMvc.perform(get(location)).andDo(print()).andExpect(status().isOk());
     }
     @Test
-    @Disabled
     public void editWorksFine() throws Exception{
-       // this.mockMvc.perform(post("/users?name=VeryVeryTestName")).andExpect(status().is(HttpStatus.CREATED.value()));
-
-       // this.mockMvc.perform(put("/users?name=VeryVeryTestName")).andExpect(status().is(HttpStatus.CREATED.value()));
+        ObjectMapper objectMapper = new ObjectMapper();
+        String uri = "/users?name=VeryVeryTestUser";
+        MvcResult mvcResult = mockMvc
+                .perform(post(uri).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        mockMvc.perform(get(location).contentType(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        mockMvc
+                .perform(put(location + "?newName=VeryNewTestEntity")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+        mvcResult = mockMvc
+                .perform(get(location).contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+        UserProjection newUser = objectMapper.readValue(
+                mvcResult
+                .getResponse()
+                .getContentAsString(), UserProjection.class);
+        assertEquals(newUser.name, "VeryNewTestEntity");
     }
-
-    @Test
-    void getUserById() {
+}
+class UserProjection{
+    public String id;
+    public String name;
+    public UserProjection(String id, String name){
+        this.id = id;
+        this.name = name;
     }
-
-    @Test
-    void getContactsByUserId() {
-    }
-
-    @Test
-    void addUser() {
-    }
-
-    @Test
-    void deleteUser() {
-    }
-
-    @Test
-    void editUser() {
-    }
+    public UserProjection(){}
 }
